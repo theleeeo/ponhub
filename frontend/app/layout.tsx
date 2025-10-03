ed44c93eebcc20b788b08da2a75b32cd2795b0e3
Leonard Forsberg <leonard.e.forsberg@gmail.com>
2025-10-03 21:52:52 +0200

    remove geist

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,7 +1,5 @@
 import type React from "react";
 import type { Metadata } from "next";
-import { GeistSans } from "geist/font/sans";
-import { GeistMono } from "geist/font/mono";
 import { Suspense } from "react";
 import "./globals.css";
 
@@ -17,7 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`font-sans ${GeistSans.variable} ${GeistMono.variable}`}>
+      <body>
         <Suspense fallback={<div>Loading...</div>}>{children}</Suspense>
       </body>
     </html>
